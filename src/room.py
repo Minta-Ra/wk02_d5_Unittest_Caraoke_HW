ab41f89f45373c77948e1f9fb46120d6f884c533
Raminta Razauskaite <r.razauskaite@gmail.com>
2021-11-19 12:47:28 +0000

    Check-in and check-out guests

--- src/room.py
+++ src/room.py
@@ -1,2 +1,14 @@
 class Room:
-    pass
+    def __init__(self, name):
+        self.name = name
+        self.guest_list = []
+        self.song_list = []
+
+
+    def check_in_guest(self, guest):
+        self.guest_list.append(guest)
+        return len(self.guest_list)
+
+    def check_out_guest(self, guest):
+        self.guest_list.remove(guest)
+        return len(self.guest_list)
--- tests/room_test.py
+++ tests/room_test.py
@@ -1,6 +1,31 @@
 import unittest
+from src.room import Room
 from src.guest import Guest
 from src.song import Song
 
+
 class TestRoom(unittest.TestCase):
-    pass
+
+    def setUp(self):
+        # Create Room object:
+        self.room_1 = Room("Radio Star")
+        self.room_2 = Room("The Singing Stop")
+
+        # Create Guest object
+        self.guest_1 = Guest("Martha Smith")
+        self.guest_2 = Guest("John Higins")
+
+        # Create Song object
+        self.song_1 = Song("Dancing in the Moonlight", 3.45)
+
+
+    def test_to_check_in_guest(self):
+        expected = 1
+        actual = self.room_1.check_in_guest(self.guest_2)
+        self.assertEqual(expected, actual)
+
+    def test_to_check_out_guest(self):
+        self.room_1.check_in_guest(self.guest_2)
+        expected = 0
+        actual = self.room_1.check_out_guest(self.guest_2)
+        self.assertEqual(expected, actual)
